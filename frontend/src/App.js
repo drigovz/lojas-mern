0b9874274bde0a6ae106d62c29055e8e750a4397
Rodrigo Vaz <rodrigodp2014@gmail.com>
2020-10-14 20:17:28 -0300

    Iniciando o projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Header from  './components/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+        <h1>Bem-Vindo as Lojas Mern</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
